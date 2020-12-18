a5fe2b5ee0feea883876de613aef8693b93ac70e
Jeff Wiedemann <jwiedemann@splunk.com>
2020-12-18 14:45:35 -0600

    Updates

--- deployment-configs/ds_deploy.sh
+++ deployment-configs/ds_deploy.sh
@@ -3,3 +3,6 @@
 #copy GDI inputs apps to DS deployment apps folder to prep for delivery to hosts via UF phone home
 cp -R gdi-configs/deployment-server/deployment-apps/inputs/* /opt/splunk/etc/deployment-apps/.
 cp -R gdi-configs/deployment-server/deployment-apps/tas/* /opt/splunk/etc/deployment-apps/.
+
+#Copy the server class configs over to apps to help deployment to UFs
+cp -R gdi-configs/deployment-server/deployment-classes/* /opt/splunk/etc/apps/
---
+++ gdi-configs/deployment-server/deployment-apps/tas/Splunk_TA_windows/README.txt
@@ -0,0 +1,4 @@
+Splunk Add-on for Microsoft Windows version 8.0.0
+Copyright (C) 2020 Splunk Inc. All Rights Reserved.
+
+For documentation, see: http://docs.splunk.com/Documentation/WindowsAddOn/latest
