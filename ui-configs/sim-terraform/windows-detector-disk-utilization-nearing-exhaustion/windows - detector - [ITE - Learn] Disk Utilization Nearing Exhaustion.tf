fab312ff0bab8acd06e5d74d52c88bab0131fa67
Jeff Wiedemann <jwiedemann@splunk.com>
2021-06-22 11:58:57 -0500

    fixed fat finger

--- ui-configs/sim-terraform/windows-detector-disk-utilization-nearing-exhaustion/windows - detector - [ITE - Learn] Disk Utilization Nearing Exhaustion.tf
+++ ui-configs/sim-terraform/windows-detector-disk-utilization-nearing-exhaustion/windows - detector - [ITE - Learn] Disk Utilization Nearing Exhaustion.tf
@@ -34,15 +34,15 @@ variable "sim_prefix" {
 # Customized Detector and Message Body
 
 resource "signalfx_detector" "countdown" {
-  name         = "${var.sim_prefix} Host Storage Monitoring"
+  name         = "${var.sim_prefix} Host Disk Utilization Nearing Exhaustion"
   description  = "Alerts when increasing disk.utilization is projected to increase to 100 in 48 hours"
   program_text = <<-EOF
   from signalfx.detectors.countdown import countdown
   A = data('disk.utilization', filter=filter('host', '*') and filter('mountpoint', '*')).publish(label='A')
-  countdown.hours_left_stream_incr_detector(stream=A, maximum_capacity=100, lower_threshold=48, fire_lasting=lasting('12m', 0.95), clear_threshold=60, clear_lasting=lasting('12m', 0.95), use_double_ewma=False).publish("${var.sim_prefix} Host Storage Monitoring")
+  countdown.hours_left_stream_incr_detector(stream=A, maximum_capacity=100, lower_threshold=48, fire_lasting=lasting('12m', 0.95), clear_threshold=60, clear_lasting=lasting('12m', 0.95), use_double_ewma=False).publish("${var.sim_prefix} Host Disk Utilization Nearing Exhaustion")
   EOF
   rule {
-    detect_label       = "${var.sim_prefix} Host Storage Monitoring"
+    detect_label       = "${var.sim_prefix} Host Disk Utilization Nearing Exhaustion"
     severity           = "Critical"
     parameterized_body = var.message_body
   }
