6c9a536ad73bcd9689ca20e2fa35cca84d8a4bfc
jeffwiedemann <34551412+jeffwiedemann@users.noreply.github.com>
2021-07-14 10:18:52 -0500

    Fixed host to host.name to comply with OTEL
    Fixed host to host.name to comply with OTEL metata

--- ui-configs/sim-terraform/windows-detector-disk-utilization-nearing-exhaustion/windows - detector - [ITE - Learn] Disk Utilization Nearing Exhaustion.tf
+++ ui-configs/sim-terraform/windows-detector-disk-utilization-nearing-exhaustion/windows - detector - [ITE - Learn] Disk Utilization Nearing Exhaustion.tf
@@ -38,7 +38,7 @@ resource "signalfx_detector" "countdown" {
   description  = "Alerts when increasing disk.utilization is projected to increase to 100 in 48 hours"
   program_text = <<-EOF
   from signalfx.detectors.countdown import countdown
-  A = data('disk.utilization', filter=filter('host', '<HOSTS-TO-CHECK>') and filter('mountpoint', '<DISKS-TO-CHECK>')).publish(label='A')
+  A = data('disk.utilization', filter=filter('host.name', '<HOSTS-TO-CHECK>') and filter('mountpoint', '<DISKS-TO-CHECK>')).publish(label='A')
   countdown.hours_left_stream_incr_detector(stream=A, maximum_capacity=100, lower_threshold=48, fire_lasting=lasting('12m', 0.95), clear_threshold=60, clear_lasting=lasting('12m', 0.95), use_double_ewma=False).publish("${var.sim_prefix} Host Disk Utilization Nearing Exhaustion")
   EOF
   rule {
